2e472152a250ab9165a3df9ceb3a52bda94954da
Vikki9121 <vignesh24csengineer@gmail.com>
2023-09-25 22:53:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function getq(){
+    document.getElementById("pa").innerHTML=" ";
+    var queri=document.getElementById("query").value;
+    var url="https://api.nasa.gov/planetary/apod?api_key=4iWhuYckFgwjufu2vCPzb3OB5B5gdqUeXw2PJvF3&date=";
+    var fu=url+queri;
+    fetch(fu).
+    then(resp => resp.json())
+    .then(data => {
+        dat=data['explanation'];
+     dat1=data['url'];
+     xyz=document.getElementById('im');
+xyz.style.cssText="width:80%;height:50%;margin:10%;margin-top:3%;";
+        
+       xyz.setAttribute("src",dat1);
+      
+        var exp=document.createElement("p");
+        pa.style.cssText="text-align:center; color:white;";
+       var xc=document.createTextNode(dat);
+       document.getElementById("pa").appendChild(xc);
+
+
+
+    });
+
+
+
+
+
+}
+
